936dd863f1b14ce975a0b39fc2c3cd427cb11aa2
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-12-02 18:51:11 +0100

    Day 2: Cube Conundrum

--- AdventOfCode2023/Helper.cs
+++ AdventOfCode2023/Helper.cs
@@ -1,10 +1,10 @@
 ﻿namespace AdventOfCode2023;
 internal static class Helper
 {
-    public static List<string> ReadTextFile(string fileName)
+    public static string[] ReadTextFile(string fileName)
     {
         string projectDirectory = AppDomain.CurrentDomain.BaseDirectory.Split(@"\bin")[0];
         string filePath = $"{projectDirectory}/Inputs/{fileName}.txt";
-        return File.ReadAllLines(filePath).ToList();
+        return File.ReadAllLines(filePath);
     }
 }

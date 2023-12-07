a90e0502260db4498dce0dd2510adac8c1dc0c11
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-12-07 21:19:06 +0100

    Day 4 part 1

---
+++ AdventOfCode2023/Day4.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode2023;
+
+internal class Day4
+{
+    public class Card
+    {
+        public List<int> WinningNumbers { get; set; } = new();
+        public List<int> OwnNumbers { get; set; } = new();
+    }
+
+    public void Run()
+    {
+        var input = Helper.ReadTextFile("4");
+        Part1(input);
+    }
+
+    public void Part1(string[] input)
+    {
+        var cards = input.Select(ParseCard);
+        var pointsSum = cards.Select(GetCardPoints).Sum();
+        Console.WriteLine("1 - Sum of card points: " + pointsSum);
+    }
+
+
+
+    private Card ParseCard(string line)
+    {
+        var parts = line.Split(':', '|');
+        return new Card
+        {
+            WinningNumbers = ExtractNumbers(parts[1]),
+            OwnNumbers = ExtractNumbers(parts[2])
+        };
+    }
+
+    private List<int> ExtractNumbers(string input)
+        => input
+            .Split(' ')
+            .Where(x => x.Length > 0)
+            .Select(n => int.Parse(n.Trim()))
+            .ToList();
+
+    private int GetCardPoints(Card card)
+    {
+        int matches = GetCardMatchesCount(card);
+        return (int)(Math.Pow(2, matches) / 2);
+    }
+
+    private int GetCardMatchesCount(Card card)
+        => card.OwnNumbers
+            .Where(n => card.WinningNumbers.Contains(n))
+            .Count();
+}

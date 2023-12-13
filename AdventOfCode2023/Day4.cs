d72bf25516ec5b6d1f0c7c2427be333c5c016592
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-12-13 22:11:40 +0100

    Day 4 part 2

--- AdventOfCode2023/Day4.cs
+++ AdventOfCode2023/Day4.cs
@@ -4,6 +4,7 @@ internal class Day4
 {
     public class Card
     {
+        public int Id { get; set; }
         public List<int> WinningNumbers { get; set; } = new();
         public List<int> OwnNumbers { get; set; } = new();
     }
@@ -12,6 +13,7 @@ internal class Day4
     {
         var input = Helper.ReadTextFile("4");
         Part1(input);
+        Part2(input);
     }
 
     public void Part1(string[] input)
@@ -21,6 +23,19 @@ internal class Day4
         Console.WriteLine("1 - Sum of card points: " + pointsSum);
     }
 
+    public void Part2(string[] input)
+    {
+        var cards = input.Select(ParseCard).ToList();
+        int highestCardId = cards.Select(c => c.Id).Max();
+        for (int i = 1; i <= highestCardId; i++)
+        {
+            var currentSet = cards.Where(c => c.Id == i);
+            var bonusCards = currentSet.SelectMany(c => GetBonusCards(c, cards)).ToList();
+            cards.AddRange(bonusCards);
+        }
+        Console.WriteLine("2 - Number of cards: " + cards.Count);
+    }
+
 
 
     private Card ParseCard(string line)
@@ -28,10 +43,14 @@ internal class Day4
         var parts = line.Split(':', '|');
         return new Card
         {
+            Id = ExtractId(parts[0]),
             WinningNumbers = ExtractNumbers(parts[1]),
             OwnNumbers = ExtractNumbers(parts[2])
         };
     }
+
+    private int ExtractId(string input)
+        => int.Parse(input.Replace("Card", "").Trim());
 
     private List<int> ExtractNumbers(string input)
         => input
@@ -50,4 +69,16 @@ internal class Day4
         => card.OwnNumbers
             .Where(n => card.WinningNumbers.Contains(n))
             .Count();
+
+    private IEnumerable<Card> GetBonusCards(Card card, IEnumerable<Card> baseCards)
+    {
+        var bonusCards = new List<Card>();
+        int bonusCardsCount = GetCardMatchesCount(card);
+        for (int i = 1; i <= bonusCardsCount; i++)
+        {
+            var cardToCopy = baseCards.First(c => c.Id == card.Id + i);
+            bonusCards.Add(cardToCopy);
+        }
+        return bonusCards;
+    }
 }

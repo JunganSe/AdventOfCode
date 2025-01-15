ca5cb9b0f00faff5f557a3c3b2e3552bfed21492
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-01-15 22:40:02 +0100

    Add helper class

---
+++ AdventOfCode2024/AdventOfCode2024/Helper.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2024;
+
+internal static class Helper
+{
+    public static string[] ReadTextFile(string fileName)
+    {
+        string projectDirectory = AppDomain.CurrentDomain.BaseDirectory.Split(@"\bin")[0];
+        string filePath = $"{projectDirectory}/Inputs/{fileName}.txt";
+        return File.ReadAllLines(filePath);
+    }
+}
